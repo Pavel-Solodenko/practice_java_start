a417b3b3e474fe594950b3840a5ef1da7d10b44b
Pavel-Solodenko <103494794+Pavel-Solodenko@users.noreply.github.com>
2022-04-12 15:08:38 +0300

    Done
    Use cycles to draw wallpaper. The number of lines is entered from the keyboard.

---
+++ lesson_3/cycles/level1/Task_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of lines");
+		int lines = sc.nextInt();
+		sc.close();
+		for (int i = 0; i < lines; i++) {
+			System.out.println("***+++***+++***+++***");
+		}
+	}
+}
